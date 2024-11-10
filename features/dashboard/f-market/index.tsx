9f3d0763ce9a516854dc4626748ef9d305aa2e1e
aminhp93 <minhpn.org.ec@gmail.com>
2024-11-10 16:19:06 +0700

    [86cwz2035] add test data fmarket

--- features/dashboard/f-market/index.tsx
+++ features/dashboard/f-market/index.tsx
@@ -1,9 +1,10 @@
+/* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 // Import libaries
 import { useEffect, useState } from "react";
 import HighchartsReact from "highcharts-react-official";
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import Highcharts from "highcharts";
 import { GridColDef } from "@mui/x-data-grid-premium";
 
@@ -20,7 +21,46 @@ import {
   formatNumber,
   getCellClassName,
 } from "@/features/dashboard/fireant/fireant-watchlists/utils";
-import { mapData } from "./utils";
+import { mapData, getNthSmallestDateInMonth, INIT_DATE } from "./utils";
+import dayjs, { Dayjs } from "dayjs";
+
+export const processDates = (
+  data: Dayjs[][],
+  monthIndex = 0,
+  currentCombination: Dayjs[] = [],
+  currentIndex = 1,
+  totalCombinations = 1
+): { combinations: Dayjs[][]; currentIndex: number } => {
+  // Base case: if we've processed all months
+  if (monthIndex >= data.length) {
+    console.log(
+      `${currentIndex}/${totalCombinations}`
+      // currentCombination.map((date) => date.format("YYYY-MM-DD"))
+    );
+    return {
+      combinations: [currentCombination],
+      currentIndex: currentIndex + 1,
+    };
+  }
+
+  const month = data[monthIndex];
+  const results: Dayjs[][] = [];
+
+  // Recursive case: generate combinations for each date in the current month
+  for (const date of month) {
+    const result = processDates(
+      data,
+      monthIndex + 1,
+      [...currentCombination, date],
+      currentIndex,
+      totalCombinations * month.length
+    );
+    results.push(...result.combinations);
+    currentIndex = result.currentIndex;
+  }
+
+  return { combinations: results, currentIndex };
+};
 
 const FMarket = () => {
   const config = useConfigStore((state) => state.config);
@@ -30,14 +70,159 @@ const FMarket = () => {
   const [rows, setRows] = useState<any>([]);
   const [options] = useState<Highcharts.Options>(getDefaultOptions());
 
+  const test = () => {
+    console.log("test");
+    (async () => {
+      try {
+        const res = await FMarketService.getNavHistory();
+        const result: {
+          [key: number]: {
+            all: any[];
+            n: number;
+            lastData: any;
+          };
+        } = {};
+        for (let n = 1; n < 9; n++) {
+          const data = res.data.filter((i) => i.navDate > INIT_DATE);
+
+          // uniq data by key navDate, get the first one
+          const validatedData = Object.values(
+            data.reduce((acc: { [key: string]: (typeof data)[0] }, cur) => {
+              if (!acc[cur.navDate]) {
+                acc[cur.navDate] = cur;
+              }
+              return acc;
+            }, {})
+          );
+          const listDate = validatedData.map((i) => dayjs(i.navDate));
+
+          const smallestDateInMonth = getNthSmallestDateInMonth(listDate, n);
+
+          const mappedRes = mapData(res, smallestDateInMonth);
+          //  find the last data that have navTest not undefined
+          let lastData;
+          // find the last data that have navTest not undefined
+          for (let i = mappedRes.length - 1; i >= 0; i--) {
+            if (mappedRes[i].navTest) {
+              lastData = mappedRes[i];
+              break;
+            }
+          }
+
+          result[n] = {
+            all: mappedRes,
+            n,
+            lastData,
+          };
+        }
+        console.log("result", result);
+        // filter result by navTestPercent in lastData
+        const sortedResult = Object.values(result).sort((a, b) => {
+          return b.lastData.navTestPercent - a.lastData.navTestPercent;
+        });
+        console.log("sortedResult", sortedResult);
+        setRows(sortedResult[0].all);
+
+        // setRows(mappedRes);
+
+        // const xxx = mapListDate(res);
+        // console.log("xxx", xxx);
+
+        // // get largest navTestPercent of each array in xxx
+
+        // const result = [];
+        // const totalLoops = processDates(xxx);
+        // totalLoops.combinations.forEach((combination, index) => {
+        //   console.log("index", index);
+        //   const all = mapData(
+        //     res,
+        //     combination.map((date) => date.format("YYYY-MM-DD"))
+        //   );
+        //   let lastData;
+        //   // find the last data that have navTest not undefined
+        //   for (let i = all.length - 1; i >= 0; i--) {
+        //     if (all[i].navTest) {
+        //       lastData = all[i];
+        //       break;
+        //     }
+        //   }
+
+        //   result.push({
+        //     all,
+        //     lastData,
+        //   });
+        // });
+
+        // // sort by largest navTestPercent
+        // const sortedResult = result.sort((a, b) => {
+        //   return b.lastData.navTestPercent - a.lastData.navTestPercent;
+        // });
+
+        // console.log("totalLoops", totalLoops, sortedResult);
+        // setRows(sortedResult[0].all);
+      } catch (err: any) {
+        // eslint-disable-next-line no-console
+        console.error(err);
+      }
+    })();
+  };
+
   useEffect(() => {
     (async () => {
       try {
         const res = await FMarketService.getNavHistory();
+        const data = res.data.filter((i) => i.navDate > INIT_DATE);
 
-        const mappedRes = mapData(res);
+        // uniq data by key navDate, get the first one
+        const validatedData = Object.values(
+          data.reduce((acc: { [key: string]: (typeof data)[0] }, cur) => {
+            if (!acc[cur.navDate]) {
+              acc[cur.navDate] = cur;
+            }
+            return acc;
+          }, {})
+        );
+        const listDate = validatedData.map((i) => dayjs(i.navDate));
+
+        const smallestDateInMonth = getNthSmallestDateInMonth(listDate, 9);
+
+        const mappedRes = mapData(res, smallestDateInMonth);
+
+        // find the largset navTestPercent in each item navDate with same month and year
+        // group data by field navDate with same year and month
+        // const groupedData = Object.values(
+        //   mappedRes.reduce((acc: { [key: string]: typeof mappedRes }, item) => {
+        //     const date = dayjs(item.navDate);
+        //     const key = date.format("YYYY-MM");
+        //     if (!acc[key]) {
+        //       acc[key] = [item];
+        //     } else {
+        //       acc[key].push(item);
+        //       acc[key].sort((a, b) => a.navDate.localeCompare(b.navDate));
+        //     }
+        //     return acc;
+        //   }, {})
+        // );
+        // console.log("groupedData1", groupedData);
+        // in each group, find the largest navChangePercent
+        // const xxx2 = [];
+        // groupedData.forEach((group) => {
+        //   const largest = group.reduce((acc, cur) => {
+        //     return acc.navChangePercent < cur.navChangePercent ? acc : cur;
+        //   });
+        //   console.log("largest", largest);
+        //   xxx2.push(largest);
+        // });
+
+        // const x2 = mapData(
+        //   res,
+        //   xxx2.map((i) => i.navDate)
+        // );
+        // console.log("x2", x2);
+
         setRawData(mappedRes);
 
+        // setRows(x2);
         setRows(mappedRes);
       } catch (err: any) {
         // eslint-disable-next-line no-console
@@ -77,14 +262,38 @@ const FMarket = () => {
               "& .color-yellow": {
                 color: "rgb(204, 170, 0)",
               },
+              "& .background-light-green": {
+                backgroundColor: "rgb(230, 255, 230)",
+              },
+              "& .background-light-yellow": {
+                backgroundColor: "rgb(255, 255, 230)",
+              },
             }}
           >
-            <Table columns={columns} rows={rows} />
+            <Table
+              columns={columns}
+              rows={rows}
+              initialState={{
+                columns: {
+                  columnVisibilityModel: {
+                    // Hide columns status and traderName, the other columns will remain visible
+                    id: false,
+                  },
+                },
+              }}
+            />
           </Box>
         )}
         {config.displayType === "chart" && (
           <HighchartsReact highcharts={Highcharts} options={options} />
         )}
+        <Button
+          onClick={() => {
+            test();
+          }}
+        >
+          Test
+        </Button>
       </Box>
     </Box>
   );
@@ -94,6 +303,11 @@ export default FMarket;
 
 const columns: GridColDef[] = [
   { field: "id", headerName: "id", width: 100, groupable: false },
+  {
+    field: "navDate",
+    headerName: "navDate",
+    minWidth: 120,
+  },
   {
     field: "nav",
     headerName: "nav",
@@ -124,31 +338,57 @@ const columns: GridColDef[] = [
       return params.value ? formatNumber(Number(params.value.toFixed(2))) : "-";
     },
   },
-  {
-    field: "navDate",
-    headerName: "navDate",
-    minWidth: 120,
-  },
+
   {
     field: "navAll",
     headerName: "navAll",
     minWidth: 120,
+    cellClassName: "background-light-green",
     renderCell: (params) => {
       return params.value ? formatNumber(Number(params.value.toFixed(2))) : "-";
     },
   },
   {
-    field: "navFirstOfMonth",
-    headerName: "navFirstOfMonth",
+    field: "navAllReal",
+    headerName: "navAllReal",
     minWidth: 120,
+    cellClassName: "background-light-green",
     renderCell: (params) => {
       return params.value ? formatNumber(Number(params.value.toFixed(2))) : "-";
     },
   },
   {
-    field: "navLastOfMonth",
-    headerName: "navLastOfMonth",
+    field: "navAllPercent",
+    headerName: "navAllPercent",
     minWidth: 120,
+    cellClassName: "background-light-green",
+    renderCell: (params) => {
+      return params.value ? formatNumber(Number(params.value.toFixed(2))) : "-";
+    },
+  },
+  {
+    field: "navTest",
+    headerName: "navTest",
+    minWidth: 120,
+    cellClassName: "background-light-yellow",
+    renderCell: (params) => {
+      return params.value ? formatNumber(Number(params.value.toFixed(2))) : "-";
+    },
+  },
+  {
+    field: "navTestReal",
+    headerName: "navTestReal",
+    minWidth: 120,
+    cellClassName: "background-light-yellow",
+    renderCell: (params) => {
+      return params.value ? formatNumber(Number(params.value.toFixed(2))) : "-";
+    },
+  },
+  {
+    field: "navTestPercent",
+    headerName: "navTestPercent",
+    minWidth: 120,
+    cellClassName: "background-light-yellow",
     renderCell: (params) => {
       return params.value ? formatNumber(Number(params.value.toFixed(2))) : "-";
     },
--- features/dashboard/f-market/utils.ts
+++ features/dashboard/f-market/utils.ts
@@ -1,109 +1,142 @@
 /* eslint-disable no-console */
 import { NavHistoryResponse } from "@/@core/services/f-market/schema";
-import dayjs from "dayjs";
+import dayjs, { Dayjs } from "dayjs";
 
-export function mapData(result: NavHistoryResponse) {
+const INIT_NAV = 1;
+const ADDED_NAV = 1;
+
+type DateMap = { [key: string]: dayjs.Dayjs[] };
+
+export const INIT_DATE = "2019-01-01";
+
+export const mapListDate = (result: NavHistoryResponse) => {
   // validate data
-  const data = result.data;
+  const data = result.data.filter((i) => i.navDate > INIT_DATE);
+
   // uniq data by key navDate, get the first one
-  const validatedData = Object.values(data.reduce((acc: { [key: string]: typeof data[0] }, cur) => {
-    if (!acc[cur.navDate]) {
-      acc[cur.navDate] = cur;
+  const validatedData = Object.values(
+    data.reduce((acc: { [key: string]: (typeof data)[0] }, cur) => {
+      if (!acc[cur.navDate]) {
+        acc[cur.navDate] = cur;
+      }
+      return acc;
+    }, {})
+  );
+
+  // group data by field navDate with same year and month
+  return Object.values(
+    validatedData.reduce((acc: DateMap, item) => {
+      const date = dayjs(item.navDate);
+      const key = date.format("YYYY-MM");
+      if (!acc[key]) {
+        acc[key] = [date];
+      } else {
+        acc[key].push(date);
+        acc[key].sort((a, b) => a.diff(b));
+      }
+      return acc;
+    }, {} as DateMap)
+  );
+};
+
+export const getNthSmallestDateInMonth = (
+  listDate: Dayjs[],
+  n: number
+): string[] => {
+  return Object.values(
+    listDate.reduce((acc: DateMap, date) => {
+      const key = date.format("YYYY-MM");
+      if (!acc[key]) {
+        acc[key] = [date];
+      } else {
+        acc[key].push(date);
+        acc[key].sort((a, b) => a.diff(b));
+      }
+      return acc;
+    }, {})
+  )
+    .map((dates) =>
+      dates.length >= n ? dates[n - 1].format("YYYY-MM-DD") : null
+    )
+    .filter((date) => date !== null);
+};
+
+export function mapData(result: NavHistoryResponse, listTestDate: string[]) {
+  // validate data
+  const data = result.data.filter((i) => i.navDate > INIT_DATE);
+
+  // uniq data by key navDate, get the first one
+  const validatedData = Object.values(
+    data.reduce((acc: { [key: string]: (typeof data)[0] }, cur) => {
+      if (!acc[cur.navDate]) {
+        acc[cur.navDate] = cur;
+      }
+      return acc;
+    }, {})
+  );
+
+  let navAll = INIT_NAV;
+  let navAllReal = INIT_NAV;
+  let navAllPercent = 0;
+
+  let navTest = INIT_NAV;
+  let navTestReal = INIT_NAV;
+  let navTestPercent = 0;
+  let navTestPreviousNav: number;
+
+  // Example usage:
+  const smallestDateInMonth = listTestDate;
+
+  const xxx = validatedData.map((i, index) => {
+    // Validate data
+    const currentNav = i.nav;
+    let navChange;
+    let navChangePercent;
+    let test = false;
+
+    if (index === 0) {
+      navTestPreviousNav = i.nav;
     }
-    return acc;
-  }, {}));
 
-  console.log('validatedData', validatedData)
+    if (index > 0) {
+      const previousNav = validatedData[index - 1].nav;
+      navChange = currentNav - previousNav;
+      navChangePercent = (navChange / previousNav) * 100;
+    }
 
-
-    const INIT_NAV = 1;
-    let navAll = INIT_NAV;
-    let navFirstOfMonth = INIT_NAV;
-    let navLastOfMonth = INIT_NAV;
-
-    const listDate = validatedData.map(i => dayjs(i.navDate))
-
- // Define the type for the accumulator object
- type DateMap = { [key: string]: dayjs.Dayjs };
-
- // filter only the smallest date in same month and year. For example, [2022-01-01, 2022-01-02, 2022-01-03] will return [2022-01-01]
- const smallestDateInMonth = Object.values(listDate
-     .reduce((acc: DateMap, date) => {
-         const key = date.format("YYYY-MM");
-         if (!acc[key] || date.isBefore(acc[key])) {
-             acc[key] = date;
-         }
-         return acc;
-     }, {}))
-     .map(date => date.format("YYYY-MM-DD"));
-
-
- // filter only the largest date in same month and year. For example, [2022-01-01, 2022-01-02, 2022-01-03] will return [2022-01-03]
- const largestDateInMonth = Object.values(listDate
-     .reduce((acc: DateMap, date) => {
-         const key = date.format("YYYY-MM");
-         if (!acc[key] || date.isAfter(acc[key])) {
-             acc[key] = date;
-         }
-         return acc;
-     }, {}))
-     .map(date => date.format("YYYY-MM-DD"));
-
-     console.log('smallestDateInMonth', smallestDateInMonth, 'largestDateInMonth', largestDateInMonth)
-
-    // eslint-disable-next-line no-console
-    console.log("result", result);
-    const xxx = validatedData.map((i, index) => {
-      // Validate data
-      const currentNav = i.nav;
-      let navChange;
-      let navChangePercent;
-      let firstOfMonth = false
-        let lastOfMonth = false
-      
-      
-      if (index > 0) {
-        const previousNav = validatedData[index - 1].nav;
-        navChange = currentNav - previousNav;
-        navChangePercent = (navChange / previousNav) * 100;
-      }
-
-      if (navChangePercent !== undefined) {
-
-      navAll = navAll * (1 + navChangePercent / 100);
-      }
+    if (navChangePercent !== undefined) {
+      navAll = navAll * (1 + navChangePercent / 100) + ADDED_NAV;
+      navAllReal = navAllReal + ADDED_NAV;
+      navAllPercent = ((navAll - navAllReal) / navAllReal) * 100;
+    }
 
     //   if date in smallestDateInMonth, set check to true
-        if (smallestDateInMonth.includes(dayjs(i.navDate).format("YYYY-MM-DD"))) {
-            firstOfMonth = true
-            if (navChangePercent !== undefined) {
-                navFirstOfMonth = navFirstOfMonth * (1 + navChangePercent / 100);
-            }
-        }
+    if (smallestDateInMonth.includes(dayjs(i.navDate).format("YYYY-MM-DD"))) {
+      test = true;
+      if (navChangePercent !== undefined) {
+        navTest =
+          navTest * (1 + (i.nav - navTestPreviousNav) / navTestPreviousNav) +
+          ADDED_NAV;
+        navTestReal = navTestReal + ADDED_NAV;
+        navTestPercent = ((navTest - navTestReal) / navTestReal) * 100;
 
-    //   if date in largestDateInMonth, set check to true
-        if (largestDateInMonth.includes(dayjs(i.navDate).format("YYYY-MM-DD"))) {
-            lastOfMonth = true
-            if (navChangePercent !== undefined) {
-                navLastOfMonth = navLastOfMonth * (1 + navChangePercent / 100);
-            }
-        }
+        navTestPreviousNav = i.nav;
+      }
+    }
 
-  
-      return {
-        ...i,
-        navChange,
-        navChangePercent,
-        navAll,
-        firstOfMonth,
-        navFirstOfMonth: firstOfMonth ? navFirstOfMonth : undefined,
-        lastOfMonth,
-        navLastOfMonth: lastOfMonth ? navLastOfMonth : undefined,
-      };
-    });
+    return {
+      ...i,
+      navChange,
+      navChangePercent,
+      navAll,
+      navAllReal,
+      navAllPercent,
+      test,
+      navTest: index === 0 || test ? navTest : undefined,
+      navTestReal: index === 0 || test ? navTestReal : undefined,
+      navTestPercent: index === 0 || test ? navTestPercent : undefined,
+    };
+  });
 
-    console.log('xxx', xxx)
-
-    return xxx
-  }
-  
+  return xxx;
+}
